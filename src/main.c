46fd7ace749d7db1103a5ea6d3ed715704f36d37
Richard Kocián <richardkocian4@gmail.com>
2023-12-10 20:56:36 +0100

    feat: formatting

--- src/main.c
+++ src/main.c
@@ -17,8 +17,7 @@
 
 #define SHT3X_I2C_ADDRESS 0x44
 
-void sh31_i2c_master_init()
-{
+void sh31_i2c_master_init() {
     i2c_config_t i2c_config;
     i2c_config.mode = I2C_MODE_MASTER;
     i2c_config.sda_io_num = SHT31_I2C_SDA;
@@ -31,8 +30,7 @@ void sh31_i2c_master_init()
     i2c_driver_install(SHT31_I2C, i2c_config.mode, 0, 0, 0);
 }
 
-void sht3x_read_data(float *temperature, float *humidity)
-{
+void sht3x_read_data(float *temperature, float *humidity) {
     uint8_t command[] = {0x2C, 0x06}; // page 10 high repeatability measurement
 
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
@@ -61,12 +59,11 @@ void sht3x_read_data(float *temperature, float *humidity)
     int32_t raw_temperature = (data[0] << 8) | data[1];
     int32_t raw_humidity = (data[3] << 8) | data[4];
 
-    *temperature = -45.0 + 175.0 * (float)raw_temperature / 65535.0; // doc page 13
-    *humidity = 100.0 * (float)raw_humidity / 65535.0;
+    *temperature = -45.0 + 175.0 * (float) raw_temperature / 65535.0; // doc page 13
+    *humidity = 100.0 * (float) raw_humidity / 65535.0;
 }
 
-void app_main()
-{
+void app_main() {
     sh31_i2c_master_init();
 
     SSD1306_t dev;
@@ -80,14 +77,11 @@ void app_main()
 
     bool mode = false;
 
-    while (1)
-    {
+    while (1) {
         float temperature, humidity;
 
-        if (mode)
-        {
-            for (int i = 0; i < 6; i++)
-            {
+        if (mode) {
+            for (int i = 0; i < 6; i++) {
                 sht3x_read_data(&temperature, &humidity);
 
                 char temperatureString[16]; // Adjust the size based on your needs
@@ -106,11 +100,8 @@ void app_main()
                 ssd1306_display_text_x3(&dev, 3, temperatureString, 16, false);
                 vTaskDelay(500 / portTICK_PERIOD_MS); // Pause for 1 second
             }
-        }
-        else
-        {
-            for (int i = 0; i < 6; i++)
-            {
+        } else {
+            for (int i = 0; i < 6; i++) {
                 sht3x_read_data(&temperature, &humidity);
 
                 char temperatureString[16]; // Adjust the size based on your needs
