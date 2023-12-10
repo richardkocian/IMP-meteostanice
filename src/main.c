392de9a2cd7aa9243ad3c62f433259a45ceab67c
Richard Kocián <richardkocian4@gmail.com>
2023-12-10 16:13:30 +0100

    feat: Functional sht31

--- src/main.c
+++ src/main.c
@@ -1,1 +1,70 @@
-void app_main() {}
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/i2c.h"
+
+#define I2C_MASTER_SCL_IO 16
+#define I2C_MASTER_SDA_IO 17
+#define I2C_MASTER_NUM I2C_NUM_0
+#define I2C_MASTER_FREQ_HZ 100000
+
+#define SHT3X_I2C_ADDRESS 0x44
+
+void i2c_master_init() {
+    i2c_config_t i2c_config = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = I2C_MASTER_FREQ_HZ,
+    };
+    i2c_param_config(I2C_MASTER_NUM, &i2c_config);
+    i2c_driver_install(I2C_MASTER_NUM, i2c_config.mode, 0, 0, 0);
+}
+
+void sht3x_read_data(float *temperature, float *humidity) {
+    uint8_t command[] = {0x2C, 0x06}; // High precision measurement command
+
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (SHT3X_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+    i2c_master_write(cmd, command, sizeof(command), true);
+    i2c_master_stop(cmd);
+
+    i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 1000 / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
+
+    vTaskDelay(20 / portTICK_PERIOD_MS); // Wait for measurement to complete
+
+    // Read 6 bytes of data (temperature and humidity)
+    uint8_t data[6];
+    cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (SHT3X_I2C_ADDRESS << 1) | I2C_MASTER_READ, true);
+    i2c_master_read(cmd, data, sizeof(data), I2C_MASTER_ACK);
+    i2c_master_stop(cmd);
+
+    i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 1000 / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
+
+    // Convert raw data to temperature and humidity
+    int16_t raw_temperature = (data[0] << 8) | data[1];
+    int16_t raw_humidity = (data[3] << 8) | data[4];
+
+    *temperature = -45.0 + 175.0 * (float)raw_temperature / 65535.0;
+    *humidity = 100.0 * (float)raw_humidity / 65535.0;
+}
+
+void app_main() {
+    i2c_master_init();
+
+    while (1) {
+        float temperature, humidity;
+        sht3x_read_data(&temperature, &humidity);
+
+        printf("Teplota: %.2f °C, Vlhkost: %.2f %%\n", temperature, humidity);
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Pause for 1 second
+    }
+}
