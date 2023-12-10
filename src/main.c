9b86ea365c2db1044b0193ea683fea17ee4494e5
Richard Kocián <richardkocian4@gmail.com>
2023-12-10 20:53:05 +0100

    feat: Refactor and periodically refreshing display

--- src/main.c
+++ src/main.c
@@ -5,34 +5,35 @@
 
 #include <stdlib.h>
 #include <string.h>
-#include "esp_log.h"
 
 #include "ssd1306.h"
 
 #define tag "SSD1306"
 
-#define I2C_MASTER_SCL_IO 25
-#define I2C_MASTER_SDA_IO 26
-#define I2C_MASTER_NUM I2C_NUM_1
-#define I2C_MASTER_FREQ_HZ 100000
+#define SHT31_I2C_SCL 25
+#define SHT31_I2C_SDA 26
+#define SHT31_I2C I2C_NUM_1
+#define SHT31_I2C_FREQ 100000 // = 100 MHz
 
 #define SHT3X_I2C_ADDRESS 0x44
 
-void sh31_i2c_master_init() {
-    i2c_config_t i2c_config = {
-        .mode = I2C_MODE_MASTER,
-        .sda_io_num = I2C_MASTER_SDA_IO,
-        .sda_pullup_en = GPIO_PULLUP_ENABLE,
-        .scl_io_num = I2C_MASTER_SCL_IO,
-        .scl_pullup_en = GPIO_PULLUP_ENABLE,
-        .master.clk_speed = I2C_MASTER_FREQ_HZ,
-    };
-    i2c_param_config(I2C_MASTER_NUM, &i2c_config);
-    i2c_driver_install(I2C_MASTER_NUM, i2c_config.mode, 0, 0, 0);
+void sh31_i2c_master_init()
+{
+    i2c_config_t i2c_config;
+    i2c_config.mode = I2C_MODE_MASTER;
+    i2c_config.sda_io_num = SHT31_I2C_SDA;
+    i2c_config.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    i2c_config.scl_io_num = SHT31_I2C_SCL;
+    i2c_config.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    i2c_config.master.clk_speed = SHT31_I2C_FREQ;
+
+    i2c_param_config(SHT31_I2C, &i2c_config);
+    i2c_driver_install(SHT31_I2C, i2c_config.mode, 0, 0, 0);
 }
 
-void sht3x_read_data(float *temperature, float *humidity) {
-    uint8_t command[] = {0x2C, 0x06}; // High precision measurement command
+void sht3x_read_data(float *temperature, float *humidity)
+{
+    uint8_t command[] = {0x2C, 0x06}; // page 10 high repeatability measurement
 
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
     i2c_master_start(cmd);
@@ -40,7 +41,7 @@ void sht3x_read_data(float *temperature, float *humidity) {
     i2c_master_write(cmd, command, sizeof(command), true);
     i2c_master_stop(cmd);
 
-    i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 1000 / portTICK_PERIOD_MS);
+    i2c_master_cmd_begin(SHT31_I2C, cmd, 1000 / portTICK_PERIOD_MS);
     i2c_cmd_link_delete(cmd);
 
     vTaskDelay(20 / portTICK_PERIOD_MS); // Wait for measurement to complete
@@ -53,7 +54,7 @@ void sht3x_read_data(float *temperature, float *humidity) {
     i2c_master_read(cmd, data, sizeof(data), I2C_MASTER_ACK);
     i2c_master_stop(cmd);
 
-    i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 1000 / portTICK_PERIOD_MS);
+    i2c_master_cmd_begin(SHT31_I2C, cmd, 1000 / portTICK_PERIOD_MS);
     i2c_cmd_link_delete(cmd);
 
     // Convert raw data to temperature and humidity
@@ -64,52 +65,72 @@ void sht3x_read_data(float *temperature, float *humidity) {
     *humidity = 100.0 * (float)raw_humidity / 65535.0;
 }
 
-
-
-
-void app_main() {
+void app_main()
+{
     sh31_i2c_master_init();
 
     SSD1306_t dev;
 
-	ESP_LOGI(tag, "INTERFACE is i2c");
-	ESP_LOGI(tag, "CONFIG_SDA_GPIO=%d",CONFIG_SDA_GPIO);
-	ESP_LOGI(tag, "CONFIG_SCL_GPIO=%d",CONFIG_SCL_GPIO);
-	ESP_LOGI(tag, "CONFIG_RESET_GPIO=%d",CONFIG_RESET_GPIO);
-	i2c_master_init(&dev, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO, CONFIG_RESET_GPIO);
+    i2c_master_init(&dev, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO, CONFIG_RESET_GPIO);
 
-	ESP_LOGI(tag, "Panel is 128x64");
+    ssd1306_init(&dev, 128, 64);
 
-	ssd1306_init(&dev, 128, 64);
+    ssd1306_clear_screen(&dev, false);
+    ssd1306_contrast(&dev, 0xff);
 
-	ssd1306_clear_screen(&dev, false);
-	ssd1306_contrast(&dev, 0xff);
-	//ssd1306_display_text_x3(&dev, 0, "Hello", 5, false);
-	//vTaskDelay(3000 / portTICK_PERIOD_MS);
+    bool mode = false;
 
-    while (1) {
+    while (1)
+    {
         float temperature, humidity;
-        sht3x_read_data(&temperature, &humidity);
 
-        
-        char temperatureString[16]; // Adjust the size based on your needs
-        char humidityString[16]; // Adjust the size based on your needs
+        if (mode)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                sht3x_read_data(&temperature, &humidity);
 
+                char temperatureString[16]; // Adjust the size based on your needs
+                char humidityString[16];    // Adjust the size based on your needs
 
-        // Convert float to string with two decimal places
-        sprintf(temperatureString, "%.2f", temperature);
+                // Convert float to string with two decimal places
+                sprintf(temperatureString, "%.2f", temperature);
 
-        sprintf(humidityString, "%.2f", humidity);
+                sprintf(humidityString, "%.2f", humidity);
 
-        printf(temperatureString);
-        printf(" : ");
-        printf(humidityString);
-        printf("\n");
+                printf(temperatureString);
+                printf(" : ");
+                printf(humidityString);
+                printf("\n");
 
-        ssd1306_display_text_x3(&dev, 0, temperatureString, 16, false);
-        ssd1306_display_text_x3(&dev, 5, humidityString, 16, false);
+                ssd1306_display_text_x3(&dev, 3, temperatureString, 16, false);
+                vTaskDelay(500 / portTICK_PERIOD_MS); // Pause for 1 second
+            }
+        }
+        else
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                sht3x_read_data(&temperature, &humidity);
 
+                char temperatureString[16]; // Adjust the size based on your needs
+                char humidityString[16];    // Adjust the size based on your needs
 
-        vTaskDelay(1000 / portTICK_PERIOD_MS); // Pause for 1 second
+                // Convert float to string with two decimal places
+                sprintf(temperatureString, "%.2f", temperature);
+
+                sprintf(humidityString, "%.2f", humidity);
+
+                printf(temperatureString);
+                printf(" : ");
+                printf(humidityString);
+                printf("\n");
+                ssd1306_display_text_x3(&dev, 3, humidityString, 16, false);
+                vTaskDelay(500 / portTICK_PERIOD_MS); // Pause for 1 second
+            }
+        }
+
+        ssd1306_clear_screen(&dev, false);
+        mode = !mode;
     }
 }
